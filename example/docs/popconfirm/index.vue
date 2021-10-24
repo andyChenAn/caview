fab80d3e6b5dcd60a861cfa5ecabc2937c47150b
andy <160149808@qq.com>
2021-10-24 22:40:17 +0800

    修改readme

--- example/docs/popconfirm/index.vue
+++ example/docs/popconfirm/index.vue
@@ -5,58 +5,55 @@
     <br />
     <br />
     <div class="title2 mb15">代码演示</div>
-    <button @click="onClick">click</button>
-    <div v-if="showPopconfirm">
-      <div class="inner">
-        <Popconfirm placement="topLeft">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">topLeft</button>
-        </Popconfirm>
-        <Popconfirm @confirm="onConfirm" @cancel="onCancel">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">top</button>
-        </Popconfirm>
-        <Popconfirm placement="topRight">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">topRight</button>
-        </Popconfirm>
-        <Popconfirm placement="leftTop">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">leftTop</button>
-        </Popconfirm>
-        <Popconfirm placement="left">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">left</button>
-        </Popconfirm>
-        <Popconfirm placement="leftRight">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">leftRight</button>
-        </Popconfirm>
-        <Popconfirm placement="bottomLeft">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">bottomLeft</button>
-        </Popconfirm>
-        <Popconfirm placement="bottom">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">bottom</button>
-        </Popconfirm>
-        <Popconfirm placement="bottomRight">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">bottomRight</button>
-        </Popconfirm>
-        <Popconfirm placement="rightTop">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">rightTop</button>
-        </Popconfirm>
-        <Popconfirm placement="right">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">right</button>
-        </Popconfirm>
-        <Popconfirm placement="rightBottom">
-          <template v-slot:title>Are you sure delete this task?</template>
-          <button class="btn">rightBottom</button>
-        </Popconfirm>
-      </div>
+    <div class="inner">
+      <Popconfirm placement="topLeft">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">topLeft</button>
+      </Popconfirm>
+      <Popconfirm @confirm="onConfirm" @cancel="onCancel">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">top</button>
+      </Popconfirm>
+      <Popconfirm placement="topRight">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">topRight</button>
+      </Popconfirm>
+      <Popconfirm placement="leftTop">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">leftTop</button>
+      </Popconfirm>
+      <Popconfirm placement="left">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">left</button>
+      </Popconfirm>
+      <Popconfirm placement="leftRight">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">leftRight</button>
+      </Popconfirm>
+      <Popconfirm placement="bottomLeft">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">bottomLeft</button>
+      </Popconfirm>
+      <Popconfirm placement="bottom">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">bottom</button>
+      </Popconfirm>
+      <Popconfirm placement="bottomRight">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">bottomRight</button>
+      </Popconfirm>
+      <Popconfirm placement="rightTop">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">rightTop</button>
+      </Popconfirm>
+      <Popconfirm placement="right">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">right</button>
+      </Popconfirm>
+      <Popconfirm placement="rightBottom">
+        <template v-slot:title>Are you sure delete this task?</template>
+        <button class="btn">rightBottom</button>
+      </Popconfirm>
     </div>
   </div>
 </template>
@@ -65,14 +62,9 @@ export default {
   data() {
     return {
       show: false,
-      show2: false,
-      showPopconfirm : true
     };
   },
   methods: {
-    onClick() {
-      this.showPopconfirm = false;
-    },
     onConfirm() {},
     onCancel() {},
   },
