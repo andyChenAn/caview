a74bea1acf2be486b0497fa1e79e3e10ab2bbcd7
andy <160149808@qq.com>
2021-12-14 22:29:21 +0800

    开发tabs组件

--- example/docs/tabs/index.vue
+++ example/docs/tabs/index.vue
@@ -15,6 +15,16 @@
         </Tabs>
       </div>
     </div>
+    <div class="inner">
+      <div style="margin-bottom: 20px">基本使用</div>
+      <div>
+        <Tabs activeKey="1" @prevClick="prevClick">
+          <TabPane tab="tab1" key="1">tab1 content</TabPane>
+          <TabPane tab="tab2234" key="2">tab2 content</TabPane>
+          <TabPane tab="tab3" key="3">tab3 content</TabPane>
+        </Tabs>
+      </div>
+    </div>
   </div>
 </template>
 <script>
