503d5ab0c465c24ca123ae9ed19e126fa443862b
andy <160149808@qq.com>
2021-12-25 09:46:36 +0800

    修复popover的bug

--- src/components/tag/index.js
+++ src/components/tag/index.js
@@ -1,3 +1,23 @@
 export default {
-  render () {}
+  props : {
+    // 标签颜色
+    color : {
+      type : String,
+      default : ''
+    },
+    // 是否显示标签隐藏按钮
+    closable : {
+      type : Boolean,
+      default : false
+    },
+    // 是否显示标签
+    visible : {
+      type : Boolean,
+      default : false
+    }
+  },
+  render () {
+    const h = this.$createElement;
+    
+  }
 }
