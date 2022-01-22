c09d0341706dd281c17d9797bb62858b08b05410
andy <160149808@qq.com>
2022-01-22 14:45:06 +0800

    开发checkbox组件

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,17 +1,43 @@
+import classNames from "classnames";
+
 export default {
   props : {
     checked : Boolean,
     defaultChecked : Boolean,
     autoFocus : Boolean,
-    disabled : Boolean
+    disabled : Boolean,
+    prefixCls : String,
+    type : {
+      type : String,
+      default : 'checkbox'
+    }
   },
   render () {
     const h = this.$createElement;
+    const { prefixCls , type , disabled } = this.$props;
     return h(
       'span',
       {
-        
-      }
+        class : classNames(prefixCls)
+      },
+      [
+        h(
+          'input',
+          {
+            attrs : {
+              type : type,
+              disabled : disabled
+            },
+            class : classNames(prefixCls + '-input')
+          }
+        ),
+        h(
+          'span',
+          {
+            class : classNames(prefixCls + '-content')
+          }
+        )
+      ]
     )
   }
 }
